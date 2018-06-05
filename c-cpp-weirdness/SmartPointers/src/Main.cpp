2562d6ad13da7502cab968352d52788910ef52df
Andrey <andreybicalho@gmail.com>
2018-06-04 21:00:10 -0300

    unique pointer

--- c-cpp-weirdness/SmartPointers/src/Main.cpp
+++ c-cpp-weirdness/SmartPointers/src/Main.cpp
@@ -86,16 +86,17 @@ int main()
 	{
 		std::shared_ptr<Entity> p1 = std::make_shared<Entity>("entityOne"); // using default deleter: operator delete
 		std::shared_ptr<Entity> p2 = std::shared_ptr<Entity>(new Entity("entityTwo"),
-			[](Entity* p) {std::cout << "Custom deleting. " << std::endl; delete p; }); // using lambda
+			[](Entity* p) { std::cout << "Custom deleting. " << std::endl; delete p; }); // using lambda
 
 		// practical use of custom deleting with lambdas
-		std::shared_ptr<Entity> p3(new Entity[3]); // dog[1] and dog[2] have memory leaks!
-		std::shared_ptr<Entity> p4(new Entity[3], [](Entity* p) {delete[] p; }); // all 3 Entity will be deleted when p4 goes out of scope
+		// std::shared_ptr<Entity> p3(new Entity[3]); // dog[1] and dog[2] have memory leaks! this will crash
+		// the safer way is by using lambdas:
+		std::shared_ptr<Entity> p4(new Entity[3], [](Entity* p) {delete[] p; }); // all 3 entities will be deleted when p4 goes out of scope
 	}
 
 
 	// example 7: shared pointers, returning the raw pointer
-	std::cout << std::endl << "\nExample 6:" << std::endl;
+	std::cout << std::endl << "\nExample 7:" << std::endl;
 	{
 		std::shared_ptr<Entity> p1 = std::make_shared<Entity>("entityOne"); // using default deleter: operator delete
 		
@@ -110,12 +111,70 @@ int main()
 
 
 
+
+
+
+
+
+	// example 8: unique pointers, light weight smart pointer. (can not be copied)
+	std::cout << std::endl << "\nExample 8:" << std::endl;
+	{
+		std::unique_ptr<Entity> p1 = std::make_unique<Entity>("entityOne"); 
+		// std::unique_ptr<Entity> p2 = p1; this can't be done
+	}
+
+
+	// example 9: unique pointers, releasing auto management
+	std::cout << std::endl << "\nExample 9:" << std::endl;
+	{
+		std::unique_ptr<Entity> p1 = std::make_unique<Entity>("entityOne");
+		
+		Entity* e = p1.release(); // p1 give up ownership
+
+		if(!p1)
+		{ 
+			std::cout << "\np1 is empty.\n";
+		}
+
+		e->printName();
+	}
+	// entityOne is not deleted
+
+	// example 10: unique pointers, transferring ownership
+	std::cout << std::endl << "\nExample 10:" << std::endl;
+	{
+		std::unique_ptr<Entity> p1 = std::make_unique<Entity>("entityOne");
+		std::unique_ptr<Entity> p2 = std::make_unique<Entity>("entityTwo");
+
+		p2->printName();
+		p2 = std::move(p1);
+		p2->printName();
+		// consequences:
+		// 1. entityTwo is destroyed
+		// 2. p1 becomes empty
+		// 3. p2 owns entityOne
+	}
+
+	// example 11: unique pointers, deleting arrays
+	std::cout << std::endl << "\nExample 10:" << std::endl;
+	{
+		std::unique_ptr<Entity[]> ps(new Entity[3]);
+		// don't need to make a lambda function to delete the array... all 3 entities will be automatically deleted
+	}
+
+
+
+
+
+
+
+
+
+
 	// TODO (andrey): casting smart pointers to a different pointer
 	// static_pointer_cast
 	// dynamic_pointer_cast
 	// const_pointer_cast
-
-
 
 
 	std::cout << std::endl << std::endl << "Press any key to exit..." << std::endl;
